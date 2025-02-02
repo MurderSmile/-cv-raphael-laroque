31828498706bd1a6b009872e9bc3745d51189a7b
Raphael Laroque <raphael.laroque@yahoo.fr>
2025-02-02 13:01:11 +0100

    MAJ Softskills 1.01

--- src/Pages/Cv/index.jsx
+++ src/Pages/Cv/index.jsx
@@ -3,33 +3,31 @@ import Profil from "./CvSections/Profil";
 import Experiences from "./CvSections/Experiences";
 import Formations from "./CvSections/Formations";
 import Competences from "./CvSections/Competences";
-import SoftSkills from "./CvSections/SoftSkills";
+import SoftSkills from "./CvSections/Softskills";
 import Interets from "./CvSections/Interets";
 import Realisations from "./CvSections/Realisations";
 import Project from "./CvSections/Project";
-
 
 import { useContext } from "react";
 
 import { ProjectContext } from "../../Utils/Context";
 
-
 function Cv() {
-    const { project } = useContext(ProjectContext)
+  const { project } = useContext(ProjectContext);
 
-    return (
-        <main id="Cv">
-            <Presentation />
-            <Profil />
-            <Experiences />
-            <Formations />
-            <Competences />
-            <SoftSkills />
-            <Interets />
-            <Realisations />
-            {project && project != null ? <Project /> : null}
-        </main>
-    )
+  return (
+    <main id="Cv">
+      <Presentation />
+      <Profil />
+      <Experiences />
+      <Formations />
+      <Competences />
+      <SoftSkills />
+      <Interets />
+      <Realisations />
+      {project && project != null ? <Project /> : null}
+    </main>
+  );
 }
 
-export default Cv
+export default Cv;
