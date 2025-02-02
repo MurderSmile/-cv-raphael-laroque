64ad39137b2e12a8aa9f9e2e593b850a09fdf293
Raphael Laroque <raphael.laroque@yahoo.fr>
2025-02-02 15:47:18 +0100

    Softskills 1.06

--- src/Pages/Cv/index.jsx
+++ src/Pages/Cv/index.jsx
@@ -1,12 +1,12 @@
-import Presentation from "./Cvsections/Presentaion";
-import Profil from "./Cvsections/Profil";
-import Experiences from "./Cvsections/Experiences";
-import Formations from "./Cvsections/Formations";
-import Competences from "./Cvsections/Competences";
+import Presentation from "./Cvsections/Presentaion/index.jsx";
+import Profil from "./Cvsections/Profil/index.jsx";
+import Experiences from "./Cvsections/Experiences/index.jsx";
+import Formations from "./Cvsections/Formations/index.jsx";
+import Competences from "./Cvsections/Competences/index.jsx";
 import Softskills from "./Cvsections/Softskills/index.jsx";
-import Interets from "./Cvsections/Interets";
-import Realisations from "./Cvsections/Realisations";
-import Project from "./Cvsections/Project";
+import Interets from "./Cvsections/Interets/index.jsx";
+import Realisations from "./Cvsections/Realisations/index.jsx";
+import Project from "./Cvsections/Project/index.jsx";
 
 import { useContext } from "react";
 
